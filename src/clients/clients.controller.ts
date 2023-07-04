79bc86a128175805c940388523f8a413f25243be
Yulia <you@example.com>
2023-07-04 15:48:27 +0300

    5.2

--- src/clients/clients.controller.ts
+++ src/clients/clients.controller.ts
@@ -8,14 +8,18 @@ import {
   Delete,
   UseGuards,
   ParseIntPipe,
+  UseInterceptors,
+  UploadedFile,
 } from '@nestjs/common';
 import { ClientsService } from './clients.service';
 import { CreateClientDto } from './dto/create-client.dto';
 import { UpdateClientDto } from './dto/update-client.dto';
 import { ApiTags, ApiResponse, ApiBearerAuth } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
+import { FileInterceptor } from '@nestjs/platform-express';
 // import { ClientsModule } from './clients.module';
 import { Client as clientEntity } from './entities/client.entity';
+import { LoggingInterceptor } from 'src/interceptors/logging.interceptors';
 
 @ApiTags('Clients')
 @ApiBearerAuth()
@@ -41,6 +45,7 @@ export class ClientsController {
   }
 
   @Get(':id')
+  @UseInterceptors(LoggingInterceptor)
   findOne(@Param('id', ParseIntPipe) id: string) {
     return this.clientsService.findOneById(+id);
   }
@@ -53,5 +58,11 @@ export class ClientsController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.clientsService.remove(+id);
+  }
+
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file'))
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
   }
 }
